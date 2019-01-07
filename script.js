3fa21dfd72ceb59505f79c55f1625c21c954eddf
AndyWebster <andy.webster.online@gmail.com>
2019-01-07 13:44:21 +0000

    Added about section
    New about section with 3d cube navigation

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ const pink = style.getPropertyValue("--main-pink");
 const gold = style.getPropertyValue("--main-gold");
 const brown = style.getPropertyValue("--main-brown");
 
-document.getElementById("about").style.backgroundColor = gold;
-
 document.addEventListener("scroll", function() {
   scrolling = true;
 });
@@ -32,15 +30,23 @@ window.onload = function() {
 // Dynamically adjusts Nav styling to suit current panel
 function setNavColors() {
   let currentPosition = document.scrollingElement.scrollTop;
+  let aboutYPos = document.getElementById("about-section").offsetTop;
+  let projectsYPos = document.getElementById("projects").offsetTop;
   let footerYPos = document.getElementById("footer").offsetTop;
+
   let buffer = 50;
 
   let aboutStyle = document.getElementById("about").style;
   let worksStyle = document.getElementById("works").style;
   let contactStyle = document.getElementById("contact").style;
 
-  if (currentPosition < window.innerHeight) {
-    // SECTION 1
+  if (currentPosition < aboutYPos - buffer) {
+    // CLEARS INACTIVE TABS
+    aboutStyle.backgroundColor = "";
+    worksStyle.backgroundColor = "";
+    contactStyle.backgroundColor = "";
+  } else if (currentPosition < projectsYPos - buffer) {
+    // SECTION 1 (welcome / about)
 
     // SETS ACTIVE NAVBAR TAB COLOR, CLEARS INACTIVE TABS
     aboutStyle.backgroundColor = gold;
@@ -48,35 +54,55 @@ function setNavColors() {
     contactStyle.backgroundColor = "";
     // SET NAV BACKGROUND AND HOVER ROOT COLOR VARIABLES
     document.documentElement.style.setProperty("--nav-background", charcoal);
-    document.documentElement.style.setProperty("--nav-hover", gold);
+    document.documentElement.style.setProperty("--nav-hover", tan);
   } else if (currentPosition < footerYPos - buffer) {
-    // SECTION 2
+    // SECTION 2 (projects)
     aboutStyle.backgroundColor = "";
     worksStyle.backgroundColor = charcoal;
     contactStyle.backgroundColor = "";
     document.documentElement.style.setProperty("--nav-background", gold);
-    document.documentElement.style.setProperty("--nav-hover", charcoal);
+    document.documentElement.style.setProperty("--nav-hover", tan);
   } else {
-    // SECTION 3
+    // SECTION 3 (contact)
     aboutStyle.backgroundColor = "";
     worksStyle.backgroundColor = "";
     contactStyle.backgroundColor = gold;
     document.documentElement.style.setProperty("--nav-background", charcoal);
-    document.documentElement.style.setProperty("--nav-hover", gold);
+    document.documentElement.style.setProperty("--nav-hover", tan);
   }
 }
 
 // EXPANSION PANEL FOR PROJECT TILES
 function expandPanel(id) {
   let popup = document.getElementById(id);
-  if (popup.style.opacity == 1 && popup.style.maxHeight == "200px") {
+  if (popup.style.opacity == 1 && popup.style.maxHeight == "250px") {
     popup.style.maxHeight = 0;
     popup.style.opacity = 0;
     popup.style.paddingTop = 0;
     popup.style.paddingBottom = 0;
   } else {
     popup.style.opacity = 1;
-    popup.style.maxHeight = "200px";
+    popup.style.maxHeight = "250px";
     popup.style.paddingBottom = "1rem";
   }
 }
+
+// ABOUT CUBE
+
+var cube = document.querySelector(".cube");
+var radioGroup = document.querySelector(".radio-group");
+var currentClass = "";
+
+function changeSide() {
+  var checkedRadio = radioGroup.querySelector(":checked");
+  var showClass = "show-" + checkedRadio.value;
+  if (currentClass) {
+    cube.classList.remove(currentClass);
+  }
+  cube.classList.add(showClass);
+  currentClass = showClass;
+}
+// set initial side
+changeSide();
+
+radioGroup.addEventListener("change", changeSide);
